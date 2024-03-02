0fcbad91ff31308754d70fe9b2520798f7091980
kunyanglu <lualicia88@gmail.com>
2024-03-02 15:06:55 -0500

    compute firing rates

--- scripts/LIF_model.py
+++ scripts/LIF_model.py
@@ -48,7 +48,63 @@ def simulate_LIF(
         V[t+1] = V_inf + (V[t] - V_inf) * np.exp(-dt/tau_m)
         if V[t+1] > V_threshold and reset == True:
             V[t+1] = V_reset
-    return {"V": V, "I_e": I_e, "time": time_vector}
+
+    theoretical_firing_rate = (1000/tau_m) * \
+            (0.5 + \
+            (E_L - V_threshold + R_L * I_amplitude)/(V_threshold - V_reset))
+
+    arg = (E_L - V_reset + R_L * I_amplitude)/(E_L - V_threshold + R_L * I_amplitude)
+    I_c = (V_threshold - E_L)/R_L
+    if I_amplitude > I_c:
+        theoretical_firing_rate = (1000/tau_m) / np.log(arg)
+    else:
+        theoretical_firing_rate = 0
+
+    # compute numerical firing rates
+    spike_idx = np.where(V == V_reset)[0]
+    spike_interval = 0
+    if len(spike_idx) > 2:
+        spike_interval = dt * (spike_idx[2] - spike_idx[1])
+    if spike_interval > 0:
+        numerical_firing_rate = 1000/spike_interval
+    else:
+        numerical_firing_rate = 0
+
+    return {"V": V,
+            "I_e": I_e,
+            "time": time_vector,
+            "f_numerical": numerical_firing_rate,
+            "f_theoretical": theoretical_firing_rate
+    }
+
+
+def firing_rates_vs_curent(dt, t_dur, I_amplitudes, V_threshold, V_reset,
+        I_start, I_stop):
+    """
+    compute firing rates as a function of various current amplitudes.
+    """
+    # 1. compute V(I_e, ...)
+    # 2. compute delta_T
+    # 3. compute f
+    num_steps = int(np.ceil(t_dur/dt))
+    I_vector = np.ones(num_steps + 1)
+    firing_rates_numerical = np.array([])
+    firing_rates_theoretical = np.array([])
+
+    for I_amplitude in I_amplitudes:
+        outputs = simulate_LIF(dt, t_dur, I_start, I_stop, I_amplitude,
+            V_threshold, V_reset)
+        firing_rates_numerical = np.append(
+            firing_rates_numerical,
+            outputs["f_numerical"]
+        )
+        firing_rates_theoretical = np.append(
+            firing_rates_theoretical,
+            outputs["f_theoretical"]
+        )
+
+    return firing_rates_numerical, firing_rates_theoretical
+
 
 if __name__ == "__main__":
     dt = 0.01
@@ -58,3 +114,8 @@ if __name__ == "__main__":
     I_amplitude = 10.0   # nA
     V_threshold = -50
     V_reset = -65
+
+    outputs = simulate_LIF(dt, t_dur, I_start, I_stop, I_amplitude,
+            V_threshold, V_reset)
+    I_amplitudes = np.arange(1, 15, 0.1)
+
